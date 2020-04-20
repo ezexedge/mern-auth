3fe8821b2b8dc787d73ed0bb2cc0e168faf17f51
ezequiel <ezeedge@gmail.com>
2020-04-20 16:15:13 -0300

    production

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,9 +3,10 @@ const jwt = require("jsonwebtoken")
 const expressJwt = require("express-jwt")
 const _ = require('lodash')
 const {OAuth2Client} = require('google-auth-library')
-const fetch = require('fetch')
+const fetch = require('node-fetch')
 const sgMail = require("@sendgrid/mail")
-sgMail.setApiKey('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')
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
 /*
 exports.signup = (req,res)=>{
@@ -266,14 +267,14 @@ exports.googleLogin = (req, res) => {
         if (email_verified) {
             User.findOne({ email }).exec((err, user) => {
                 if (user) {
-                    const token = jwt.sign({ _id: user._id }, 'jfiwjfiji12233450559frfnrniri44494949jgjgjgg', { expiresIn: '7d' });
+                    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, { expiresIn: '7d' });
                     const { _id, email, name, role } = user;
                     return res.json({
                         token,
                         user: { _id, email, name, role }
                     });
                 } else {
-                    let password = email + 'jfiwjfiji12233450559frfnrniri44494949jgjgjgg';
+                    let password = email + process.env.JWT_SECRET;
                     user = new User({ name, email, password });
                     user.save((err, data) => {
                         if (err) {
@@ -282,7 +283,7 @@ exports.googleLogin = (req, res) => {
                                 error: 'User signup failed with google'
                             });
                         }
-                        const token = jwt.sign({ _id: data._id }, 'jfiwjfiji12233450559frfnrniri44494949jgjgjgg', { expiresIn: '7d' });
+                        const token = jwt.sign({ _id: data._id }, process.env.JWT_SECRET, { expiresIn: '7d' });
                         const { _id, email, name, role } = data;
                         return res.json({
                             token,
@@ -300,55 +301,52 @@ exports.googleLogin = (req, res) => {
 };
 
 
-exports.facebookLogin = (req,res)=>{
-    console.log('facebook login req.body',req.body)
+exports.facebookLogin = (req, res) => {
+    console.log('FACEBOOK LOGIN REQ BODY', req.body);
+    const { userID, accessToken } = req.body;
 
-    const {userID , accessToken} = req.body
-    const url = `https://graph.facebook.com/v2.11/${userID}/?fields=id,name,email&access_token=${accessToken}`
+    const url = `https://graph.facebook.com/v2.11/${userID}/?fields=id,name,email&access_token=${accessToken}`;
 
-    return(
-        fetch(url,{
+    return (
+        fetch(url, {
             method: 'GET'
-
         })
-        .then(response => response.json())
-        .then(response=> {
-            const {email , name } = response
-            User.findOne({email}).exec((err,user)=>{
-                if(user){
-                    const token = jwt.sign({ _id: user._id }, 'jfiwjfiji12233450559frfnrniri44494949jgjgjgg', { expiresIn: '7d' });
-                    const { _id, email, name, role } = user;
-                    return res.json({
-                        token,
-                        user: { _id, email, name, role }
-                    });
-                }else{
-
-                    let password = email + 'jfiwjfiji12233450559frfnrniri44494949jgjgjgg';
-                    user = new User({ name, email, password });
-                    user.save((err, data) => {
-                        if (err) {
-                            console.log('ERROR facebook LOGIN ON USER SAVE', err);
-                            return res.status(400).json({
-                                error: 'User signup failed with facebook'
-                            });
-                        }
-                        const token = jwt.sign({ _id: data._id }, 'jfiwjfiji12233450559frfnrniri44494949jgjgjgg', { expiresIn: '7d' });
-                        const { _id, email, name, role } = data;
+            .then(response => response.json())
+            // .then(response => console.log(response))
+            .then(response => {
+                const { email, name } = response;
+                User.findOne({ email }).exec((err, user) => {
+                    if (user) {
+                        const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, { expiresIn: '7d' });
+                        const { _id, email, name, role } = user;
                         return res.json({
                             token,
                             user: { _id, email, name, role }
                         });
-                    });
-
-                }
+                    } else {
+                        let password = email + process.env.JWT_SECRET;
+                        user = new User({ name, email, password });
+                        user.save((err, data) => {
+                            if (err) {
+                                console.log('ERROR FACEBOOK LOGIN ON USER SAVE', err);
+                                return res.status(400).json({
+                                    error: 'User signup failed with facebook'
+                                });
+                            }
+                            const token = jwt.sign({ _id: data._id }, process.env.JWT_SECRET, { expiresIn: '7d' });
+                            const { _id, email, name, role } = data;
+                            return res.json({
+                                token,
+                                user: { _id, email, name, role }
+                            });
+                        });
+                    }
+                });
             })
-        })
-        .catch(error => {
-            res.json({
-                error : 'facebook login failed'
+            .catch(error => {
+                res.json({
+                    error: 'Facebook login failed. Try later'
+                });
             })
-        })
-    )
-
-}
+    );
+};
--- server.js
+++ server.js
@@ -1,42 +1,17 @@
-const express = require('express')
-const morgan = require('morgan')
-const cors = require('cors')
-const bodyParser = require('body-parser')
-const mongoose = require('mongoose')
-require('dotenv').config()
-
-
-const app = express()
-
-mongoose.connect(process.env.DATABASE ,{
-    useNewUrlParser: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
-    useCreateIndex: true
-})
-.then(()=>console.log('db connect'))
-.catch(err => console.log('db conection error', err))
-
-
-const authRoutes = require('./routes/auth')
-const userRoutes = require('./routes/user')
-
-app.use(morgan('dev'))
-///app.use(cors())
-
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: true}))
-
-if((process.env.NODE_ENV = 'development')){
-    app.use(cors({origin : `http://localhost:3000`}))
-}
-
-
-app.use('/api',authRoutes)
-app.use('/api',userRoutes)
-
-
-const port =  process.env.PORT || 8000
-app.listen(port , ()=>{
-    console.log('funciona')
-})
+const express = require('express');
+const compression = require('compression');
+const path = require('path');
+const app = express();
+ 
+app.use(compression());
+app.use(express.static(path.join(__dirname, 'build')));
+ 
+app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+ 
+const PORT = process.env.PORT || 3000;
+ 
+app.listen(PORT, () => {
+    console.log(`App is running on port ${PORT}`);
+});
